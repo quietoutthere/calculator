ac2254e9d13322b95f84cb2bfd753868b7fad89b
Evan Oyster <oyster.evan@gmail.com>
2022-11-04 09:53:53 -0400

    operators working

--- main.js
+++ main.js
@@ -1,11 +1,46 @@
-const calc = document.getElementById('main-body');
 const calcDisplay = document.getElementById('display');
+const numbers = document.querySelector('div.numbers');
 
-calc.addEventListener('click', function(e) {
+function grabValues() {
+    const display = calcDisplay.textContent;
+    console.log(display); 
+
+    return display
+}
+
+
+const doTheMath = (event) => {
+    let grabValues = grabValues();
+    const target = event.target.id
+    if (target === '+') {
+        console.log(grabValues + 1)
+    } else if (target === '-') {
+        console.log('ya bastard')
+    } else if (target === '/') {
+        console.log('hello there')
+    } else if (target === '*') {
+        console.log('butss')
+    }
+}
+
+numbers.addEventListener('click', function(e) {
     e.preventDefault();
     buttonValue = e.target.value;
-    const node = document.createTextNode(buttonValue);
-    calcDisplay.appendChild(node);
-    console.log(calcDisplay);
+    let node = document.createTextNode(buttonValue);
+    const newValue = calcDisplay.appendChild(node);
 });
+
+
+    
+
+function clear(e) {
+    const clear = document.getElementById('clear');
+    let target = e.target.nextSibling;
+    target.remove();
+}
+
+const fireKeys = document.getElementsByClassName('operator');
+for (let keys of fireKeys) {
+    keys.addEventListener('click', doTheMath);
+}
 
