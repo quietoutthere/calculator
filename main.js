47a30e531606fac61cb78297d11065c611e3dd02
Evan Oyster <oyster.evan@gmail.com>
2022-11-06 08:58:02 -0500

    test case

--- main.js
+++ main.js
@@ -1,46 +1,64 @@
 const calcDisplay = document.getElementById('display');
 const numbers = document.querySelector('div.numbers');
+let num1; //get 1 inside here
+let num2; // get 2 inside here
+let operator; // get + in here
 
-function grabValues() {
-    const display = calcDisplay.textContent;
-    console.log(display); 
-
-    return display
+function storedValue() {
+    const grabValue = calcDisplay.textContent; 
+    return grabValue
 }
 
-
+//possible switch case
 const doTheMath = (event) => {
-    let grabValues = grabValues();
-    const target = event.target.id
+    const target = event.target.id;
+    num1 = storedValue()
     if (target === '+') {
-        console.log(grabValues + 1)
+    
     } else if (target === '-') {
-        console.log('ya bastard')
+        console.log('subtract')
     } else if (target === '/') {
-        console.log('hello there')
+        console.log('divide')
     } else if (target === '*') {
-        console.log('butss')
+        console.log('multiply')
     }
 }
 
 numbers.addEventListener('click', function(e) {
     e.preventDefault();
+    console.log(num1, num2, operator);
     buttonValue = e.target.value;
     let node = document.createTextNode(buttonValue);
     const newValue = calcDisplay.appendChild(node);
 });
 
 
-    
-
-function clear(e) {
-    const clear = document.getElementById('clear');
-    let target = e.target.nextSibling;
-    target.remove();
-}
+// function clear(e) {
+//     const clear = document.getElementById('clear');
+//     let target = e.target.parentElement.previousSibling
+//     target.remove();
+// }
 
 const fireKeys = document.getElementsByClassName('operator');
 for (let keys of fireKeys) {
     keys.addEventListener('click', doTheMath);
 }
 
+//TEST CASE
+//DESCRIPTION: Get 1 + 2 = 3
+
+//STEP 1: Click on 'button' 1
+//EXPCTATION: 1 shows up in 'display'
+//ACTUAL: IT WORKS
+
+//STEP 2: Click on 'button +
+//EXPECATION: app 'grabs' 1 and stores it to be concatinated to following 'button push'
+//ACTUAL: nothing happens
+
+//STEP 3: Click on 'button' 2
+//EXPECTATION: 2 shows up on 'display'
+//ACTUAL: '1 2' shows up on display
+
+//STEP 4: Click on 'button' =
+//EXPECTATION: 3 shows up on 'display'
+//ACTUAL: nothing happens 
