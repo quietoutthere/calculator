38f2e33e5f6ff1baf6a1b59565014620dcaf9661
Evan Oyster <oyster.evan@gmail.com>
2022-11-18 18:08:39 -0500

    return the operator

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-const calcDisplay = document.getElementById('display');
-const numbers = document.querySelector('div.numbers');
 let num1; 
 let num2; 
+let num3;
+let num4;
 let operator;
 let operator2;
 let operator3;
 let operator4; 
 
+const calcDisplay = document.getElementById('display');
+const numbers = document.querySelector('div.numbers');
 numbers.addEventListener('click', (e) => {
     e.preventDefault();
     buttonValue = e.target.value;
@@ -16,22 +18,30 @@ numbers.addEventListener('click', (e) => {
     } else if (num1 >= 1 && !operator) {
         num1 = num1 + buttonValue;
         calcDisplay.textContent = num1;
-    }  else if ((!!operator) && num1 >= 1 && num2 >= 1) {
+    } else if (!!operator && !!operator2 && num1 >=1 && num2>=1 && num3>=1) {
+        num3 = num3 + buttonValue;
+        calcDisplay.textContent = num3;
+    }  else if(!!operator && !!operator2 && num1 >=1 && num2>=1) {
+        num3 = buttonValue;
+        calcDisplay.textContent = num3;
+    } else if (!!operator && num1 >= 1 && num2 >= 1) {
         num2 = num2 + buttonValue;
         calcDisplay.textContent = num2;
-    } else if ((!!operator) && num1 >= 1) {
+    } else if (!!operator && num1 >= 1) {
         num2 = buttonValue;
         calcDisplay.textContent = num2;
-    }
-    console.log('calcDisplay:', calcDisplay.textContent)
-    console.log('buttonValue:', buttonValue)
-    console.log('num1:', num1);
-    console.log('num2:', num2);
-    console.log('operator:', operator);
-    console.log('operator2:', operator2);
-    console.log('operator3:', operator3);
-    console.log('operator4:', operator4);
-    console.log('');  
+    } 
+    // console.log('calcDisplay:', calcDisplay.textContent)
+    // console.log('buttonValue:', buttonValue)
+    // console.log('num1:', num1);
+    // console.log('num2:', num2);
+    // console.log('num3:', num3);
+    // console.log('operator:', operator);
+    // console.log('operator2:', operator2);
+    // console.log('operator3:', operator3);
+    // console.log('operator4:', operator4);
+    // console.log(''); 
+    console.log(chooseOperator)
 });
 
 const chooseOperator = (event) => {
@@ -40,11 +50,11 @@ const chooseOperator = (event) => {
         operator = target;
     } else if ((!!operator) && (!!operator2)) {
         operator3 = target;
-    } else if (operator === '+' || '-' || '/' || '*') {
+    } else if (!!operator) {
         operator2 = target;
-    } 
+    }
+        console.log(target)
 }
-
 
 function storedValue() {
     const grabValue = calcDisplay.textContent; 
@@ -53,7 +63,11 @@ function storedValue() {
 
 const equalSign = document.getElementById('=');
 equalSign.addEventListener('click', () => {
-    calcDisplay.textContent = parseInt(num1) + parseInt(num2);
+    calcDisplay.textContent = parseInt(num1) + parseInt(num2) + parseInt(num3);
+    num1 = undefined;
+    num2 = undefined;
+    num3 = undefined;
+    operator = undefined;
 })
 
 
@@ -75,24 +89,30 @@ const clearDisplay = () => {
 clearDisplay();
 
 //Test Case 
-//Description: Equal Sign concatinates num1 & num2
+//Description: get num3 to concatinate
 
-//Step 1: user presses 1
-//Expected: 1 shows up on display
-//Actual: IT WORKS
-
-//Step 2: user presses 2
-//Expected: 12 shows up on display
+//Step 1: user presses 111
+//Expected: 111 shows up on display
 //Actual: IT WORKS
 
 //Step 3: user presses +
 //Expected: + shows up in operator 
 //Actual: IT WORKS
 
-//Step 4: user presses 3
-//Expected: 3 shows up on display
+
+//Step 2: user presses 222
+//Expected: 222 shows up on display
 //Actual: IT WORKS
 
-//Step 5: user presses equal sign
-//Expected: 15 shows up on screen
-//Actual: Nothing Happens
+//Step 4: user presses +
+//Expected: + shows up in operator2 
+//Actual: IT WORKS
+
+
+//Step 5: user presses 333
+//Expected: 333 shows up on display
+//Actual: only 3 shows up on display 
+
+//Step 6: user presses equal sign
+//Expected: 666 shows up on screen
+//Actual: 333 shows up
