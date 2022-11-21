9cac3dd10809f313190e6fa6da2e03b8c6425947
Evan Oyster <oyster.evan@gmail.com>
2022-11-21 11:55:47 -0500

    get total string to parseInt()

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
 let num1; 
 let num2; 
-let num3;
-let num4;
+let total;
 let operator;
-let operator2;
-let operator3;
-let operator4; 
 
 const calcDisplay = document.getElementById('display');
 const numbers = document.querySelector('div.numbers');
@@ -13,47 +9,52 @@ numbers.addEventListener('click', (e) => {
     e.preventDefault();
     buttonValue = e.target.value;
     calcDisplay.innerText = buttonValue;
-    if (!operator && !num1) {
+    if (!operator && !num1 && !!total) {
         num1 = buttonValue;
-    } else if (num1 >= 1 && !operator) {
-        num1 = num1 + buttonValue;
-        calcDisplay.textContent = num1;
-    } else if (!!operator && !!operator2 && num1 >=1 && num2>=1 && num3>=1) {
-        num3 = num3 + buttonValue;
-        calcDisplay.textContent = num3;
-    }  else if(!!operator && !!operator2 && num1 >=1 && num2>=1) {
-        num3 = buttonValue;
-        calcDisplay.textContent = num3;
-    } else if (!!operator && num1 >= 1 && num2 >= 1) {
+        total = total + buttonValue;
+        calcDisplay.textContent = buttonValue;
+    }  else if (!operator && !num1) {
+        num1 = buttonValue;
+        total = buttonValue;
+    } else if (!!num2) {
         num2 = num2 + buttonValue;
         calcDisplay.textContent = num2;
-    } else if (!!operator && num1 >= 1) {
+        total = total + buttonValue;
+    } else if(!!operator && !!num1) {
         num2 = buttonValue;
         calcDisplay.textContent = num2;
-    } 
-    // console.log('calcDisplay:', calcDisplay.textContent)
-    // console.log('buttonValue:', buttonValue)
-    // console.log('num1:', num1);
-    // console.log('num2:', num2);
-    // console.log('num3:', num3);
-    // console.log('operator:', operator);
-    // console.log('operator2:', operator2);
-    // console.log('operator3:', operator3);
-    // console.log('operator4:', operator4);
-    // console.log(''); 
-    console.log(chooseOperator)
+        total = total + buttonValue;
+    } else if (!!num1) {
+        num1 = num1 + buttonValue;
+        calcDisplay.textContent = num1;
+        total = total + buttonValue;
+    } else if (!!operator && !num1) {
+        num1 = buttonValue;
+        calcDisplay.textContent = num1;
+        total = total + buttonValue;
+    }
+
+    console.log('calcDisplay:', calcDisplay.textContent)
+    console.log('buttonValue:', buttonValue)
+    console.log('num1:', num1);
+    console.log('num2:', num2);
+    console.log('operator:', operator);
+    console.log('total:', total);
+    console.log(''); 
 });
 
 const chooseOperator = (event) => {
     const target = event.target.id;
-    if (!operator) {
+    if (!!num2) {
+        calcDisplay.textContent = total;
+        total = total + target;
+        num1 = undefined;
+        num2 = undefined;
+        operator = undefined;
+    } else if (!operator || (!!operator && !!num1)) {
         operator = target;
-    } else if ((!!operator) && (!!operator2)) {
-        operator3 = target;
-    } else if (!!operator) {
-        operator2 = target;
-    }
-        console.log(target)
+        total = total + target;
+    } 
 }
 
 function storedValue() {
@@ -63,13 +64,12 @@ function storedValue() {
 
 const equalSign = document.getElementById('=');
 equalSign.addEventListener('click', () => {
-    calcDisplay.textContent = parseInt(num1) + parseInt(num2) + parseInt(num3);
+    calcDisplay.textContent = total; 
     num1 = undefined;
     num2 = undefined;
     num3 = undefined;
     operator = undefined;
 })
-
 
 const fireKeys = document.getElementsByClassName('operator');
 for (let keys of fireKeys) {
@@ -82,37 +82,51 @@ const clearDisplay = () => {
         calcDisplay.textContent = '0';
         num1 = undefined;
         num2 = undefined;
+        num3 = undefined;
         operator = undefined;
+        total = undefined;
     })
 }
 
 clearDisplay();
 
 //Test Case 
-//Description: get num3 to concatinate
+//Description: get equals sign to fire off numbers and operators
 
-//Step 1: user presses 111
-//Expected: 111 shows up on display
+//Step 1: user presses 4
+//Expected: 1 shows up on display
 //Actual: IT WORKS
 
-//Step 3: user presses +
-//Expected: + shows up in operator 
+//Step 2: user presses +
+//Expected: + shows up in operator
+//ActuaL: IT WORKS
+
+// Step 3: user presses 3
+//Expected: 2 shows up on display
+//Actual: It works
+
+//Step 4: user pesses *
+//Expected: - shows up in operator 2
+//Actual: It works
+
+//Step 5: user presses 7
+//Expected: 7 shows up on screen
 //Actual: IT WORKS
 
 
-//Step 2: user presses 222
-//Expected: 222 shows up on display
-//Actual: IT WORKS
 
-//Step 4: user presses +
-//Expected: + shows up in operator2 
-//Actual: IT WORKS
-
-
-//Step 5: user presses 333
-//Expected: 333 shows up on display
-//Actual: only 3 shows up on display 
-
-//Step 6: user presses equal sign
-//Expected: 666 shows up on screen
-//Actual: 333 shows up
+//  else if (num1 >= 1 && !operator) {
+//     num1 = num1 + buttonValue;
+//     calcDisplay.textContent = num1;
+// } else if (!!operator && !!operator2 && num1 >=1 && num2>=1 && num3>=1) {
+//     num3 = num3 + buttonValue;
+//     calcDisplay.textContent = num3;
+// }  else if(!!operator && !!operator2 && num1 >=1 && num2>=1) {
+//     num3 = buttonValue;
+//     calcDisplay.textContent = num3;
+// } else if (!!operator && num1 >= 1 && num2 >= 1) {
+//     num2 = num2 + buttonValue;
+//     calcDisplay.textContent = num2;
+// } else if (!!operator && num1 >= 1) {
+//     num2 = buttonValue;
+//     calcDisplay.textContent = num2;
